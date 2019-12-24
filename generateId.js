3d50b7d99bc16c3171bff74f02ba4fe8670cd705
Ivan Cherviakov <cherviakov.ivan@gmail.com>
2019-12-24 13:19:15 +0700

    added generateId util

---
+++ generateId.js
@@ -0,0 +1,10 @@
+const crypto = require('crypto');
+
+const alphabet = Array.from({ length: 74 }, (item, index) => String.fromCharCode(48 + index)).filter((c) => /[0-9a-zA-Z]/.test(c));
+
+const generateId = () => {
+  return Array.from({ length: 16 }, () => {
+    const numeral = parseInt(crypto.randomBytes(1).toString('hex'), 16) % alphabet.length;
+    return alphabet[numberal];
+  }).join('');
+};
